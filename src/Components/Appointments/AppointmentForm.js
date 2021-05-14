5f11f93a9291cd1e5f04923db2449e354d341c6a
hasancifitci96 <hasanhikmetciftci@gmail.com>
2021-05-14 13:44:41 +0300

    forms seem to be working just need to push them to the lists

--- src/Components/Appointments/AppointmentForm.js
+++ src/Components/Appointments/AppointmentForm.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AppointmentForm() {
+
+    const [title, setTitle] = useState('')
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+    
+    const handleTitleChange = event => {
+        setTitle(event.target.value)
+    };
+
+    const handleDateChange = event => {
+        setDate(event.target.value)
+    };
+
+    const handleTimeChange = event => {
+        setTime(event.target.value)
+    };
+    
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert(`Your state values: \n 
+                title: ${title} \n 
+                time: ${time} \n
+                You can replace this alert with your process`);
+      };
+
     return (
         <div>
-            <form method="POST">
+            <form onSubmit={handleSubmit}>
                 <div>
-                    <label>Contact Names</label>
-                    <input type='text' />
+                    <label>Appointment Title</label>
+                    <input type='text' name="title" value={title} onChange={handleTitleChange} required/>
                 </div>
                 <div>
-                    <label>Number</label>
-                    <input type='text' />
+                    <label>Date</label>
+                    <input type='date' name="date" value={date} onChange={handleDateChange} />
                 </div>
                 <div>
-                    <label>Number</label>
-                    <input type='text' />
+                    <label>Time</label>
+                    <input type='time' name="time" value={time} onChange={handleTimeChange} />
                 </div>
-                <button type='submit'>Submit</button>
-
+                <button type="submit">Submit</button>
             </form>
             <h1>This is my form for contacts</h1>
         </div>
--- src/Components/Contacts/ContactForm.js
+++ src/Components/Contacts/ContactForm.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+
+
+
+//action keyword istedigin "/afdad" adresine bir post request yolluyor      action="/example.html"
+// name="first-text-field" value="already pre-filled">  'da name'i herzaman koy bu galiba key oluyor json'daki, value kutunun icinde hazir doldurulmus alan ornek input ile
+//datalist yap user name'ler icin
+
 
 export default function ContactForm() {
+
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    
+    const handleNameChange = event => {
+        setName(event.target.value)
+    };
+
+    const handlePhoneChange = event => {
+        setPhone(event.target.value)
+    };
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value)
+    };
+    
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert(`Your state values: \n 
+                name: ${name} \n 
+                email: ${email} \n
+                You can replace this alert with your process`);
+      };
+
     return (
         <div>
-            <form method="POST">
+            <form onSubmit={handleSubmit}>
                 <div>
-                    <label>Contact Names</label>
-                    <input type='text' />
+                    <label>Contact Name</label>
+                    <input type='text' name='name' value={name} onChange={handleNameChange} required/>
                 </div>
                 <div>
-                    <label>Number</label>
-                    <input type='text' />
+                    <label>Phone Number</label>
+                    <input type='number' name='phone' value={phone} onChange={handlePhoneChange}/>
                 </div>
                 <div>
-                    <label>Number</label>
-                    <input type='text' />
+                    <label>Email</label>
+                    <input type='email' name='email' value={email} onChange={handleEmailChange}/>
                 </div>
-                <button type='submit'>Submit</button>
+                <button type="submit">Submit</button>
 
             </form>
             <h1>This is my form for contacts</h1>
